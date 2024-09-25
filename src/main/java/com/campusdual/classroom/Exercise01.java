06bba44548c00e30016741a73e0aa0950719dda1
marcos.padin@outlook.es <you@example.com>
2024-09-25 09:08:17 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -5,6 +5,5 @@ public class Exercise01 {
     public static void main(String[] args) {
         //TODO → Write: System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-
     }
 }
